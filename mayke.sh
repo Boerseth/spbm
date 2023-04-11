5c2fb25235e2f1b411d30378681f7a5299e86360
frode <frode.thorsen.borseth@softwarelab.no>
2023-04-11 20:07:26 +0200

    Reformat the mayke script a bit more

--- mayke.sh
+++ mayke.sh
@@ -23,7 +23,50 @@ media in a directory MEDIA.'
 fi
 
 
-for d in temp page; do [[ -d "$d" ]] && rm -rf "$d"; mkdir "$d"; done
+for d in temp page; do
+    [[ ! -d "$d" ]] && mkdir "$d"
+    rm -rf "$d"/*
+done
+
+
+main() {
+
+    files=$(find "$2" -type f -name '*.md')
+
+    # Verify metadata headers
+    for file in $files; do
+        header="$(head -n 5 $file | sed 's/:.*$//' | paste -s - | sed 's/\s//g')"
+        [[ $header == "---datetitlesummary..." ]] && continue
+        echo "Error in header of $file: $header" >&2
+        exit 1
+    done
+
+    # Create TOC
+    echo '<section id="#">' > temp/toc.html
+    for file in $files; do
+        echo `pandoc --template temp/toc_entry.html $file`
+    done | sort --reverse >> temp/toc.html
+    echo '</section>' >> temp/toc.html
+
+    # Create index.html
+    for file in $files; do
+        pandoc --template temp/article.html "$file"
+    done | pandoc -s --katex -f html -t html \
+        --include-in-header=temp/style.css \
+        --include-after-body=temp/script.html \
+        --metadata title="$1" \
+        -o page/index.html
+    sed -i -e '/^<\/header>$/r temp/toc.html' \
+        -e 's/^<header/<a href="#"><header/' \
+        -e 's/^<\/header>$/<\/header><\/a>/' \
+        page/index.html
+
+    # Maybe import media-files
+    [[ $# == 3 && -d $3 ]] && cp "$3"/* page/
+
+    # Clean up temporary files
+    rm -rf temp
+}
 
 
 # Create pandoc templates:
@@ -32,49 +75,49 @@ art_id='${if(date)}${date}${else}${title}${endif}'
 # Article template
 echo '
 <section id="#'"$art_id"'">
-<h1 id="'"$art_id"'">
-  ${title}
-</h1>
-
-${if(date)}
-<time>
-  ${date}
-</time>
-${endif}
-
-${body}
+    <h1 id="'"$art_id"'">
+        ${title}
+    </h1>
+    
+    ${if(date)}
+    <time>
+        ${date}
+    </time>
+    ${endif}
+    
+    ${body}
 </section>' > temp/article.html
 
 # TOC-entry template
 echo '
 <div>
-  <div>
-    ${if(date)}
-    <div class="home">
-      ${date}
+    <div>
+        <h2 class="home">
+            <a href="#'"$art_id"'">
+                ${title}
+            </a>
+        </h2>
+        
+        ${if(date)}
+        <div class="home">
+            ${date}
+        </div>
+        ${endif}
+    </div>
+    
+    ${if(summary)}
+    <div>
+        <p class="home">
+            ${summary}
+        </p>
     </div>
     ${endif}
-
-    <h2 class="home">
-      <a href="#'"$art_id"'">
-        ${title}
-      </a>
-    </h2>
-  </div>
-
-  ${if(summary)}
-  <div>
-    <p class="home">
-      ${summary}
-    </p>
-  </div>
-  ${endif}
 </div>' > temp/toc_entry.html
 
 # Style-file
 echo '
 <style>
-  section{display: none;}
+    section{display: none;}
 </style>' > temp/style.css
 
 # Script-file
@@ -95,44 +138,4 @@ window.onhashchange = () => showSection(window.location.hash || "#");
 window.onhashchange();
 </script>' > temp/script.html
 
-
-files=$(find "$2" -type f -name '*.md')
-
-
-# Verify metadata headers
-for file in $files; do
-    header="$(head -n 5 $file | sed 's/:.*$//' | paste -s - | sed 's/\s//g')"
-    [[ $header == "---datetitlesummary..." ]] && continue
-    echo "Error in header of $file: $header" >&2
-    exit 1
-done
-
-
-# Create TOC
-echo '<section id="#">' > temp/toc.html
-for file in $files; do
-    echo `pandoc --template temp/toc_entry.html $file`
-done | sort --reverse >> temp/toc.html
-echo '</section>' >> temp/toc.html
-
-
-# Create index.html
-for file in $files; do
-    pandoc --template temp/article.html "$file"
-done | pandoc -s --katex -f html -t html \
-    --include-in-header=temp/style.css \
-    --include-after-body=temp/script.html \
-    --metadata title="$1" \
-    -o page/index.html
-sed -i -e '/^<\/header>$/r temp/toc.html' \
-    -e 's/^<header/<a href="#"><header/' \
-    -e 's/^<\/header>$/<\/header><\/a>/' \
-    page/index.html
-
-
-# Maybe import media-files
-[[ $@ == 3 && -d $3 ]] && cp "$3"/* page/
-
-
-# Clean up temporary files
-rm -rf temp
+main $@
