d2206fff6474fdceb1f9cb1af3c954764657ef50
Frode T. Børseth <frode.thorsen.borseth@softwarelab.no>
2023-04-14 04:43:36 +0200

    Use CSS instead of JS to toggle visibility

--- mayke.sh
+++ mayke.sh
@@ -5,9 +5,7 @@ set -o pipefail
 if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 cd "$(dirname "$0")"
 
-
-if [[ "${1-}" =~ ^-*h(elp)?$ || $# < 2 || ! -d "$2" ]]; then
-    echo '
+HELP='
 Usage: ./mayke.sh TITLE POSTS [ MEDIA ]
 
 Use pandoc to generate a single-page blog with title TITLE from Markdown files.
@@ -19,123 +17,57 @@ The posts should each have a metadata block
         ...
 and be located in the directory specified by argument POSTS. Optionally include
 media in a directory MEDIA.'
-    exit
-fi
+if [[ "${1-}" =~ ^-*h(elp)?$ || $# < 2 || ! -d "$2" ]]; then echo "$HELP"; exit; fi
 
 
-for d in temp page; do
-    [[ ! -d "$d" ]] && mkdir "$d"
-    rm -rf "$d"/*
-done
+ART_ID='$if(date)$$date$$else$$title$$endif$'
+TEMP_POST='
+$if(full)$<section id="'"$ART_ID"'">$endif$
+$if(full)$<h1>$title$</h1>$else$<h2><a href="#'"$ART_ID"'">$title$</a></h2>$endif$
+$if(date)$<div class="post-date">$date$</div>$endif$
+$if(summary)$<div class="post-summary">$summary$</div>$endif$
+$if(full)$$body$$endif$
+$if(full)$</section>$endif$'
+# (The order of the sections becomes important: #toc must be last)
+TEMP_STYLE='
+<style>
+section { display: none; } section:target { display: block; }
+section#toc { display: block; } section:target ~ section#toc { display: none; }
+</style>'
+for d in temp build; do [[ ! -d "$d" ]] && mkdir "$d"; rm -rf "$d"/*; done
+echo "$TEMP_POST" > temp/post.html
+echo "$TEMP_STYLE" > temp/style.css
 
 
 main() {
-
-    files=$(find "$2" -type f -name '*.md')
-
     # Verify metadata headers
-    for file in $files; do
-        header="$(head -n 5 $file | sed 's/:.*$//' | paste -s - | sed 's/\s//g')"
-        [[ $header == "---datetitlesummary..." ]] && continue
+    unsorted_files=$(find "$2" -type f -name "*.md")
+    for file in $unsorted_files; do
+        header="$(echo $(head -n 4 $file | sed 's/".*"$/""/' | paste -s -) | sed 's/ //g')"
+        [[ $header == "---date:\"\"title:\"\"summary:\"\"" ]] && continue
         echo "Error in header of $file: $header" >&2
         exit 1
     done
 
-    # Create TOC
-    echo '<section id="#">' > temp/toc.html
-    for file in $files; do
-        echo `pandoc --template temp/toc_entry.html $file`
-    done | sort --reverse >> temp/toc.html
-    echo '</section>' >> temp/toc.html
+    # Sort file names for chronological TOC
+    files=$(for f in $unsorted_files; do
+        head -n 5 $f | grep '^date: ' | sed 's|^.*"\(.*\)"$|\1 '"$f"'|'
+    done | sort --reverse | sed 's/^.* //')
+
+    # Create TOC and sections
+    echo '<section id="toc">' > temp/toc
+    for f in $files; do pandoc --template temp/post.html "$f"; done >> temp/toc
+    echo '</section>' >> temp/toc
+    for f in $files; do pandoc --template temp/post.html --metadata full "$f"; done > temp/secs
 
     # Create index.html
-    for file in $files; do
-        pandoc --template temp/article.html "$file"
-    done | pandoc -s --katex -f html -t html \
-        --include-in-header=temp/style.css \
-        --include-after-body=temp/script.html \
-        --metadata title="$1" \
-        -o page/index.html
-    sed -i -e '/^<\/header>$/r temp/toc.html' \
-        -e 's/^<header/<a href="#"><header/' \
-        -e 's/^<\/header>$/<\/header><\/a>/' \
-        page/index.html
+    pandoc -s --katex -f html -t html -M title="$1" -H temp/style.css temp/secs temp/toc \
+        | sed -e 's/^<header/<a href="#toc"><header/' -e 's/^<\/header>$/<\/header><\/a>/' \
+        > build/index.html
 
     # Maybe import media-files
-    [[ $# == 3 && -d $3 ]] && cp "$3"/* page/
-
-    # Clean up temporary files
-    rm -rf temp
+    [[ $@ == 3 && -d $3 ]] && cp "$3"/* build/
 }
 
 
-# Create pandoc templates:
-art_id='${if(date)}${date}${else}${title}${endif}'
-
-# Article template
-echo '
-<section id="#'"$art_id"'">
-    <h1 id="'"$art_id"'">
-        ${title}
-    </h1>
-    
-    ${if(date)}
-    <time>
-        ${date}
-    </time>
-    ${endif}
-    
-    ${body}
-</section>' > temp/article.html
-
-# TOC-entry template
-echo '
-<div>
-    <div>
-        <h2 class="home">
-            <a href="#'"$art_id"'">
-                ${title}
-            </a>
-        </h2>
-        
-        ${if(date)}
-        <div class="home">
-            ${date}
-        </div>
-        ${endif}
-    </div>
-    
-    ${if(summary)}
-    <div>
-        <p class="home">
-            ${summary}
-        </p>
-    </div>
-    ${endif}
-</div>' > temp/toc_entry.html
-
-# Style-file
-echo '
-<style>
-    section{display: none;}
-</style>' > temp/style.css
-
-# Script-file
-echo '
-<script>
-const SECTION = "boerseth-section-key";
-const setElementDisplay = (elementId, value) => {
-    const element = document.getElementById(elementId);
-    if (element) { element.style.display = value; }
-};
-const showSection = (section) => {
-    const sectionOld = window.sessionStorage.getItem(SECTION);
-    window.sessionStorage.setItem(SECTION, section);
-    setElementDisplay(sectionOld, "none");
-    setElementDisplay(section, "block");
-};
-window.onhashchange = () => showSection(window.location.hash || "#");
-window.onhashchange();
-</script>' > temp/script.html
-
-main $@
+main "$@"
